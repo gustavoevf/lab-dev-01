466bb69219fa4c01f8310833cf3f4d6478361889
Joao Victor Temponi Daltro De Castro <1216657@sga.pucminas.br>
2023-03-01 19:39:16 -0300

    Ajuste repositório

---
+++ implementacao/codigo/Pessoa.java
@@ -0,0 +1,22 @@
+package sem Título;
+
+import java.io.*;
+import java.util.*;
+
+public class Pessoa implements Iacesso {
+
+	protected String matricula;
+	protected String nome;
+	protected String cpf;
+	protected Disciplina[] disciplinas;
+	protected Usuario usuario;
+
+	protected void login() {
+		throw new UnsupportedOperationException("The method is not implemented yet.");
+	}
+
+	protected void logout() {
+		throw new UnsupportedOperationException("The method is not implemented yet.");
+	}
+
+}
