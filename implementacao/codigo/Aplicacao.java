592bcc379cddf021d85f0dda5f2ca103b61ff92e
JVTemponi <jvictortemponi@gmail.com>
2023-03-14 12:15:46 -0300

    Ajuste chamada main

--- implementacao/codigo/Aplicacao.java
+++ implementacao/codigo/Aplicacao.java
@@ -7,52 +7,61 @@ import java.util.Arrays;
 
 public class Aplicacao {
 
-    
-	/**
-	 * @return
-	 * @throws IOException
-	 */
-	public static ArrayList<Curso> lerArquivoCurso() throws IOException {
-		try {
-			ArrayList<Curso> cursos = new ArrayList<Curso>();
-			BufferedReader buffRead = new BufferedReader(new FileReader("cursos.txt"));
-			String linha = buffRead.readLine();
-			
-			while(linha != null) {
-				String[] partes = linha.split(";");
-				if(partes.length > 0) {
-					String nomeCurso = partes[0];
-					Curso curso = new Curso(nomeCurso);
-					String disciplinas[] = Arrays.copyOfRange(partes, 1, partes.length);
-					for(String disciplina : disciplinas) {
-						curso.setDisciplinas(new Disciplina(disciplina));
-					}
-					cursos.add(curso);
-				}
-				linha = buffRead.readLine();
-			}
-			buffRead.close();
-			
-			return cursos;
-		}catch(IOException ex){
-			ex.printStackTrace();  
-			return null;
-		}
+	public static void main(String[] args) throws IOException {
+		
+		Faculdade faculdade = new Faculdade();
+		faculdade.imprimirCursos();
 	}
+	
+	
+	//Codigo antigo caso precise de algum pedaço
+	
+//  
+//	/**
+//	 * @return
+//	 * @throws IOException
+//	 */
+//	public static ArrayList<Curso> lerArquivoCurso() throws IOException {
+//		try {
+//			ArrayList<Curso> cursos = new ArrayList<Curso>();
+//			BufferedReader buffRead = new BufferedReader(new FileReader("cursos.txt"));
+//			String linha = buffRead.readLine();
+//			
+//			while(linha != null) {
+//				String[] partes = linha.split(";");
+//				if(partes.length > 0) {
+//					String nomeCurso = partes[0];
+//					Curso curso = new Curso(nomeCurso);
+//					String disciplinas[] = Arrays.copyOfRange(partes, 1, partes.length);
+//					for(String disciplina : disciplinas) {
+//						curso.setDisciplinas(new Disciplina(disciplina));
+//					}
+//					cursos.add(curso);
+//				}
+//				linha = buffRead.readLine();
+//			}
+//			buffRead.close();
+//			
+//			return cursos;
+//		}catch(IOException ex){
+//			ex.printStackTrace();  
+//			return null;
+//		}
+//	}
 	
 	/**
 	 * @param args
 	 * @throws IOException
 	 */
-	public static void main(String[] args) throws IOException {
-		ArrayList<Curso> cursos = lerArquivoCurso();
-		
-		for(Curso curso : cursos) {
-			System.out.println(curso.getNome());
-			ArrayList<Disciplina> disciplinas = curso.getDisciplinas();
-			for(Disciplina disciplina : disciplinas) {
-				System.out.println("  " + disciplina.getNome());
-			}
-		}
-	}
+	//public static void main(String[] args) throws IOException {
+		//ArrayList<Curso> cursos = lerArquivoCurso();
+	
+//		for(Curso curso : faculdade.cursos) {
+//			System.out.println(curso.getNome());
+//			ArrayList<Disciplina> disciplinas = curso.getDisciplinas();
+//			for(Disciplina disciplina : disciplinas) {
+//				System.out.println("  " + disciplina.getNome());
+//			}
+//		}
+	//}
 }
