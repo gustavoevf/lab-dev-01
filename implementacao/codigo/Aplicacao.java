d76c71a2f24862f433fba4f6fa839cc5c1cf8a64
JVTemponi <jvictortemponi@gmail.com>
2023-03-14 14:00:44 -0300

    removendo biblioteas obsoletas

--- implementacao/codigo/Aplicacao.java
+++ implementacao/codigo/Aplicacao.java
@@ -1,10 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Aplicacao {
 
